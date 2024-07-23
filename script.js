e88ec1eb49841325bed5deb73283ec4bee1fda66
Qetuose <lukasbaltrimas123@gmail.com>
2024-07-23 16:36:32 +0300

    s

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 const buttonReset = document.querySelector('.button-reset');
+const inputError = document.querySelector('.form--error');
 
 class App {
   #map;
@@ -83,8 +84,6 @@ class App {
 
     // Get data from local storage
     this._getLocalStorage();
-
-    this.testRunner();
 
     // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
@@ -158,6 +157,22 @@ class App {
       inputs.every(inp => Number.isFinite(inp));
     const allPositive = (...inputs) => inputs.every(inp => inp > 0);
 
+    const shakeAnimation = [
+      { transform: `translate(0,0)`, offset: 0 },
+      { transform: `translate(5px,0)`, offset: 0.017 },
+      { transform: `translate(0,0)`, offset: 0.035 },
+      { transform: `translate(5px,0)`, offset: 0.053 },
+      { transform: `translate(0,0)`, offset: 0.071 },
+      { transform: `translate(5px,0)`, offset: 0.089 },
+      { transform: `translate(0,0)`, offset: 0.1 },
+      { transform: `translate(0,0)`, offset: 1 },
+    ];
+
+    const animationTiming = {
+      duration: 4720,
+      iterations: 1,
+    };
+
     e.preventDefault();
 
     // Get data from form
@@ -175,8 +190,11 @@ class App {
       if (
         !validInputs(distance, duration, cadence) ||
         !allPositive(distance, duration, cadence)
-      )
-        return alert('Inputs have to be positive numbers!');
+      ) {
+        form.animate(shakeAnimation, animationTiming);
+        inputError.classList.remove('hidden');
+        return;
+      }
 
       workout = new Running([lat, lng], distance, duration, cadence);
     }
@@ -188,8 +206,11 @@ class App {
       if (
         !validInputs(distance, duration, elevation) ||
         !allPositive(distance, duration)
-      )
-        return alert('Inputs have to be positive numbers!');
+      ) {
+        form.animate(shakeAnimation, animationTiming);
+        inputError.classList.remove('hidden');
+        return;
+      }
 
       workout = new Cycling([lat, lng], distance, duration, elevation);
     }
@@ -306,23 +327,18 @@ class App {
     }
   }
 
-  _renderEditForm() {
-    const editBtns = document.querySelectorAll('.workout--button-edit');
+  _renderEditForm(e) {
+    if (e.target.classList.contains('workout--button-edit')) {
+      const workoutEl = e.target.closest('.workout');
+      if (!workoutEl) return;
 
-    editBtns.forEach(btn =>
-      btn.addEventListener('click', e => {
-        if (!this.#map) return;
+      const workout = this.#workouts.find(
+        work => work.id === workoutEl.dataset.id
+      );
 
-        const workoutEl = e.target.closest('.workout');
-        if (!workoutEl) return;
+      workoutEl.classList.add('hidden');
 
-        const workout = this.#workouts.find(
-          work => work.id === workoutEl.dataset.id
-        );
-
-        workoutEl.classList.add('hidden');
-
-        let html = `
+      let html = `
          <li class="workout workout--${workout.type}" data-id="${workout.id}"> 
         <form>
        <div class= "workout--wrapper">
@@ -345,8 +361,8 @@ class App {
         </div>
     `;
 
-        if (workout.type === 'running')
-          html += `
+      if (workout.type === 'running')
+        html += `
         <div class="workout__details">
           <span class="workout__icon">🦶🏼</span>
           <input class="edit--input value-cadence" value="${workout.cadence}"></input>
@@ -354,8 +370,8 @@ class App {
         </div>
       `;
 
-        if (workout.type === 'cycling')
-          html += `
+      if (workout.type === 'cycling')
+        html += `
         <div class="workout__details">
           <span class="workout__icon">📈</span>
           <input class="edit--input value-elevationGain" value="${workout.elevationGain}"></input>
@@ -364,7 +380,7 @@ class App {
        
       `;
 
-        html += `
+      html += `
       <div class="editContainer">
         <button type="submit" class="workout--button button-save">Save</button>
         <button class="workout--button button-cancel">Cancel</button>
@@ -373,9 +389,8 @@ class App {
         </li>
       `;
 
-        form.insertAdjacentHTML('afterend', html);
-      })
-    );
+      form.insertAdjacentHTML('afterend', html);
+    }
   }
 
   _editWorkout(e) {
@@ -491,8 +506,6 @@ class App {
     localStorage.removeItem('workouts');
     location.reload();
   }
-
-  testRunner() {}
 }
 
 const app = new App();
